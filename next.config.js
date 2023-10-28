45fd194752e0dd80cd37867e616b4978214bdd47
kutsenkomp22pv192 <113538375+kutsenkomp22pv192@users.noreply.github.com>
2023-10-28 17:11:57 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,37 @@
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
-  output: 'export',
-  basePath: '/schoolium',
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
+module.exports = withPlugins([
+  [optimizedImages, {
+    mozjpeg: {
+      quality: 80,
+    },
+    pngquant: {
+      speed: 3,
+      strip: true,
+      verbose: true,
+    },
+    imagesPublicPath: '/schoolium/_next/static/images/',
+  }],
+  {
+    basePath: '/schoolium',
+    assetPrefix: '/schoolium/',
+    env,
+  },
+]);
+// /**
+//  * @type {import('next').NextConfig}
+//  */
+// const nextConfig = {
+//   output: 'export',
+//   basePath: '/schoolium',
+//   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+//   // trailingSlash: true,
  
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
+//   // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+//   // skipTrailingSlashRedirect: true,
  
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
-}
+//   // Optional: Change the output directory `out` -> `dist`
+//   // distDir: 'dist',
+// }
  
-module.exports = nextConfig
+// module.exports = nextConfig
