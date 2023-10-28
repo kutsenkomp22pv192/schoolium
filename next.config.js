c5b5f554fb2c873b088407f7ec0515eb96b55b72
kutsenkomp22pv192 <113538375+kutsenkomp22pv192@users.noreply.github.com>
2023-10-28 17:25:06 +0300

    edit config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@
  */
 const nextConfig = {
   output: 'export',
+  basePath: '.'
   // basePath: '/schoolium',
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   // trailingSlash: true,
